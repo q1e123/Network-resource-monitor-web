d72e9fbf6145e28473144255414fa6d4a0fea27a
Robert <robertnicolaesolca@protonmail.com>
2020-09-14 16:25:13 +0300

    Added SecurityConfiguration

---
+++ src/main/java/com/q1e123/NetworkResourceMonitorWeb/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.q1e123.NetworkResourceMonitorWeb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .usersByUsernameQuery("SELECT username, Systems.machine_id, true "
+                                +"FROM Users "
+                                +"INNER JOIN Systems On Users.system_id = Systems.id "
+                                +"WHERE username = ?")
+            .authoritiesByUsernameQuery("SELECT username, IF(user_role = 1, 'ROLE_ADMIN', 'ROLE_USER') " +
+                                        "FROM Users " +
+                                        "WHERE username = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/allsystems").hasRole("ADMIN")
+                .antMatchers("/allusers").hasRole("ADMIN")
+                .and().formLogin();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncodedr() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
